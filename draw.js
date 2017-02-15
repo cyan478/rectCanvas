cd5155ae3b7803892db7ea1451090a419333a025
Harry <lilharry221@gmail.com>
2017-02-14 23:42:10 -0500

    finish

--- draw.js
+++ draw.js
@@ -1,9 +1,13 @@
+var c = document.getElementById("slate");
+var ctx = c.getContext('2d');
+ctx.beginPath()
+
 function clear(e){
-	var c = document.getElementById("slate");
 	var width = c.getAttribute("width")
 	var height = c.getAttribute("height")
 	var ctx = c.getContext('2d');
 	ctx.clearRect(0,0,width,height); 
+    firstPoint = true;
 }
 
 /* RECT */
@@ -18,26 +22,37 @@ function drawRect(e){
 	ctx.fillRect(x-25,y-35,50,70);
 }
 
+var firstPoint = true;
 
-/* CONNECT THE DOTS */
-function drawDots(e){
-	var c = document.getElementById("slate");
-	var ctx = c.getContext('2d');
+function makeCircle(x, y, r){
 
-  	var x = event.x - c.offsetLeft;
-  	var y = event.y - c.offsetTop;
-	//ctx.beginPath();
-	ctx.fillStyle = "red";
-	ctx.arc(x,y,10,0,2*Math.PI);
-	ctx.fill();
+    //x -= c.offsetLeft;
+  	//y -= c.offsetTop;
+    
+    if (firstPoint) {
+      firstPoint=false;
+    } else {
+      ctx.lineTo(x, y);
+    }
 
-	ctx.stroke();
-	//ctx.closePath();
-	//ctx.stroke();
-	ctx.moveTo(x,y)
+    ctx.moveTo(x + r,y);
+    ctx.arc(x,y,r,0,2 * Math.PI);
+    ctx.moveTo(x,y);
+    ctx.fillStyle = "#ff0000";
+    ctx.fill();
+    ctx.stroke();
+};
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return [event.clientX - rect.left,event.clientY - rect.top]
+
+};
+c.addEventListener('click', function(e){
+    var mousePos = getMousePos(c, e);
+    makeCircle(mousePos[0], mousePos[1], 10);
+                                        }, false);
 
 
-}
 
 /*
 var c = document.getElementById("slate");
@@ -49,18 +64,10 @@ ctx.lineTo(150,150);
 ctx.lineTo(300,300);
 ctx.stroke();
 
-- arcTo( centerX , centerY , radius , startAngle , endAngle )
-  - ex: arcTo(250,150,50,0,2*Math.PI);
-- arc( centerX , centerY , radius , startAngle , endAngle )
-  - draws line/radius to startAngle
-*/
-
 
 var b = document.getElementById("b");
 b.addEventListener("click",clear)
 
-var c = document.getElementById("slate");
-c.addEventListener("click",drawDots) //draw function
-
+*/
 
 
