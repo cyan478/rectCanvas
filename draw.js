fe5a0330ba8f3bb143b230c51d506a4c3a0b4126
cyan478 <cyan2@stuy.edu>
2017-02-16 15:08:48 -0500

    added animation code commented out

--- draw.js
+++ draw.js
@@ -2,6 +2,7 @@ var c = document.getElementById("slate");
 var ctx = c.getContext('2d');
 ctx.beginPath()
 
+
 function clear(e){
 	var width = c.getAttribute("width")
 	var height = c.getAttribute("height")
@@ -10,7 +11,7 @@ function clear(e){
     firstPoint = true;
 }
 
-/* RECT */
+//RECT 
 function drawRect(e){
 	var c = document.getElementById("slate");
 	var ctx = c.getContext('2d');
@@ -41,17 +42,18 @@ function makeCircle(x, y, r){
     ctx.fillStyle = "#ff0000";
     ctx.fill();
     ctx.stroke();
+    //window.requestAnimationFrame(makeCircle(250,250,150))
 };
 function getMousePos(canvas, event) {
     var rect = canvas.getBoundingClientRect();
     return [event.clientX - rect.left,event.clientY - rect.top]
 
 };
+
 c.addEventListener('click', function(e){
     var mousePos = getMousePos(c, e);
     makeCircle(mousePos[0], mousePos[1], 10);
                                         }, false);
-
 
 
 /*
@@ -67,7 +69,46 @@ ctx.stroke();
 
 var b = document.getElementById("b");
 b.addEventListener("click",clear)
-
 */
 
+//================================================ FOR ANIMATION (2/16)
+/*
+var c = document.getElementById("slate");
+var ctx = c.getContext('2d');
+//ctx.beginPath()
+
+//for drawDot
+ctx.fillStyle = "red"
+
+var rid;
+
+//var animateDot = function(){
+var x = 25;
+
+var drawDot = function(){
+	ctx.clearRect(0,0,c.width,c.height);
+	console.log("dot");
+	ctx.beginPath();
+	ctx.arc(x,c.height/2, 25, 0, Math.PI * 2);
+	x++;
+	ctx.fill();
+	rid = window.requestAnimationFrame(drawDot);
+}
+//drawDot()
+//}
+
+var stopIt = function(){
+	window.cancelAnimationFrame(rid);
+}
+
+var sb = document.getElementById('stop');
+sb.addEventListener("click", stopIt);
+
+//rid = window.requestAnimationFrame(drawDot);
+//drawDot()
+
+c.addEventListener("click",drawDot)
+//c.addEventListener("click",animateDot)
+rid = window.requestAnimationFrame(drawDot);
+*/
 
